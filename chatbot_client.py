398668de70e47a651e1a70da76aed6fee91cb705
ZParsia <fyt2cd@virginia.edu>
2025-08-20 22:45:55 -0400

    Update chatbot_client.py

--- chatbot_client.py
+++ chatbot_client.py
@@ -6,6 +6,7 @@ from naoqi import ALProxy
 DESTINY = "192.168.1.69"
 SAM = "192.168.1.128"
 JOURNEY = "192.168.1.55"
+ANGEL = "192.168.1.35"
 ROBOT_IP = JOURNEY
 PORT = 9559
 
@@ -18,6 +19,11 @@ socket.connect("tcp://localhost:5555")
 
 poller = zmq.Poller()
 poller.register(socket, zmq.POLLIN)
+
+tts = ALProxy("ALAnimatedSpeech", ROBOT_IP, PORT)
+#welcome_message = "Hello everyone, and welcome to the Collaborative Robotics Lab. My name is Journey, how can I assist you today?"
+#tts.say(welcome_message)
+
 
 cancel = False
 
@@ -38,11 +44,11 @@ while True:
                 print("Received reply {m}".format(m=message))
 
                 # print("MESSAGE: {m}".format(m=message))
-                tts = ALProxy("ALAnimatedSpeech", ROBOT_IP, PORT)
+
                 tts.say(message)
 
                 break
-            # Optional: do other stuff here
+
     except KeyboardInterrupt:
         print("Cancelled by user.")
         cancel = True
